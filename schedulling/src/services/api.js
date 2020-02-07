64ebdd524323f3fd4137057502a2fea85bc666ad
Rodrigo Borges <rodrigoborges93@gmail.com>
2020-02-07 12:36:04 +0000

    changed baseURL of API

--- schedulling/src/services/api.js
+++ schedulling/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://3333-e61703cd-9142-443f-88aa-655125d333d3.ws-us02.gitpod.io'
+    baseURL: 'https://backend-calendario-auditoria.herokuapp.com/'
 });
 
 export default api
